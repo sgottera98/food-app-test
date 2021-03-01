e300f51cf8f6a79c409fb7d3f33abe85a45e0343
Bruno Sgotti <71176979+sgottera98@users.noreply.github.com>
2021-03-01 11:09:12 -0300

    Add files via upload

--- src/components/main/sections/FirstSection.js
+++ src/components/main/sections/FirstSection.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
+import Smartphones from '../../../assets/images/Smartphones.png';
 
-// import FirstSmartphone from '../../../assets/images/smartphone-background-orange.svg';
-import FirstSmartphone from '../../../assets/images/smartphone-background-orange.png';
-import SecondSmartphone from '../../../assets/images/smartphone-background-white.png';
 
 const FirstSection = () => {
     return (
         <section >
             <div className="first-section">
-                {/* <div className="smartphones-first-section">
-                    <img className="first-smartphone" src={FirstSmartphone} alt="Smartphone reproduzindo aplicativo Bella Olonje" />
-                    <img className="second-smartphone" src={SecondSmartphone} alt="Smartphone reproduzindo aplicativo Bella Olonje" />
-                </div> */}
+                <div className="smartphones-first-section">
+                    <img src={Smartphones} alt="Smartphone reproduzindo aplicativo Bella Olonje" />
+                </div>
                 <div className="title-first-section">
                     <h2>How the app works</h2>
                 </div>
